8fda5d2bd38a92c62a314e1349ceeb9437e84d1c
Thomas Sibley <tsibley@fredhutch.org>
2022-01-28 12:29:18 -0800

    Minimal Sphinx + RTD project

---
+++ src/conf.py
@@ -0,0 +1,12 @@
+"""
+Configuration file for the Sphinx documentation builder.
+
+See <https://www.sphinx-doc.org/en/master/usage/configuration.html>.
+"""
+
+project = 'Read the Docs testbed'
+version = '20220128'
+copyright = '2022, Trevor Bedford and Richard Neher'
+author = 'The Nextstrain Team'
+
+html_theme = 'nextstrain-sphinx-theme'
